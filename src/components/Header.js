e1e91b629e6c2a03df6c76e7374f2a62ea44dace
Srivatsan <srivsriv@cisco.com>
2025-04-20 17:45:29 +0530

    login/signup auth flow with firebase and signout flow

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,38 @@
+import { signOut } from 'firebase/auth'
 import React from 'react'
+import { auth } from '../utils/firebase'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+  const navigate = useNavigate();
+  const user = useSelector(store => store.user);
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      console.log("User signed out");
+      navigate("/");
+    }).catch((error) => {
+      console.log(error);
+      console.log("Error signing out");
+      navigate("/error");
+    })
+  }
   return (
-    <div className="absolute px-4 py-2 bg-gradient-to-b from-black z-50">
-        <img className="w-52" src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-        alt="Netflix Logo"
-        />
+    <div>
+      <div className="absolute w-full px-4 py-2 bg-gradient-to-b from-black z-40">
+          <img className="w-52" src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+          alt="Netflix Logo"
+          />
+
+      </div>
+      {user && <div className="flex flex-col absolute right-0 z-50 p-2 m-2">
+          <img 
+            className="w-16"
+            src={user?.photoURL}
+            alt="Netflix Logo"
+          />
+          <button onClick={handleSignOut} className="text-blue-400 hover:text-red-400 cursor-pointer">Sign Out</button>
+      </div>}
     </div>
   )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,95 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
+import { checkValidDataForSignIn, checkValidDataForSignUp } from '../utils/validate';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { updateUser } from '../utils/userSlice';
 
 const Login = () => {
 
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+  const dispatch = useDispatch();
+
+  const email = useRef(null);
+  const password = useRef(null);
+  const fullName = useRef(null);
+
+  const navigate = useNavigate();
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
+  }
+
+  const handleButtonClick = () => {
+    // Validate the form data
+    const errorMessage = isSignInForm ? checkValidDataForSignIn(email.current.value, password.current.value): checkValidDataForSignUp(email.current.value, password.current.value, fullName.current.value);
+    setErrorMessage(errorMessage);
+
+    if (errorMessage) {
+      return;
+    }
+
+    // create a new user / sign in the user
+
+    const user = {
+      email: email.current.value,
+      password: password.current.value,
+      fullName: fullName?.current?.value
+    }
+
+    if(!isSignInForm) {
+      // Sign up logic
+
+      createUserWithEmailAndPassword(auth, user.email, user.password).then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        // update user
+
+        updateProfile(user, {
+          displayName: user.fullName, photoURL: "https://avatars.githubusercontent.com/u/24690749?s=400&u=6bb21804eef7cedb412fcea24e8444c1b9adc68a&v=4"
+        }).then(() => {
+          // Profile updated!
+          const {uid, email, displayName, photoURL} = auth.currentUser;
+        dispatch(updateUser({
+          uid,
+          email,
+          displayName,
+          photoURL
+        }));
+        navigate("/browse");
+        }).catch((error) => {
+          // An error occurred
+          // ...
+        });
+
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setErrorMessage(errorCode+ '-' + errorMessage);
+        
+      });
+
+    } else {
+      console.log("reached");
+      signInWithEmailAndPassword(auth, user.email, user.password).then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+
+        // update user
+        
+
+
+        navigate("/browse");
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setErrorMessage(errorCode+ '-' + errorMessage);
+      })
+    }
+
   }
 
   return (
@@ -17,12 +100,19 @@ const Login = () => {
             alt="Netflix Logo"
           />  
         </div>
-        <form className="flex flex-col absolute w-3/12 px-12 pt-12 pb-8 bg-black my-44 left-0 right-0 mx-auto rounded-lg bg-black/80">
+        <form onSubmit={(e) => e.preventDefault()} className="flex flex-col absolute w-3/12 px-12 pt-12 pb-8 bg-black my-44 left-0 right-0 mx-auto rounded-lg bg-black/80">
+          
           <h1 className="font-bold py-2 text-3xl text-white">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
-          {!isSignInForm &&  <input type="text" placeholder='Full Name' className="p-4  my-4 rounded-sm bg-neutral-800 text-white" />}
-          <input type="text" placeholder='Email Address' className="p-4  my-4 rounded-sm bg-neutral-800 text-white" />
-          <input type="password" placeholder='Password' className="p-4  my-4 rounded-sm bg-neutral-800 text-white" />
-          <button type="submit" className="bg-red-700 p-4 my-2 w-full rounded-lg opacity-100 text-white">{isSignInForm ? "Sign In" : "Sign Up"}</button>
+          {!isSignInForm &&  <input ref={fullName} type="text" placeholder='Full Name' className="p-4  my-4 rounded-sm bg-neutral-800 text-white" />}
+         
+          <input ref={email} type="text" placeholder='Email Address' className="p-4  my-4 rounded-sm bg-neutral-800 text-white" />
+         
+          <input ref={password} type="password" placeholder='Password' className="p-4  my-4 rounded-sm bg-neutral-800 text-white" />
+
+          <p className="p-2 text-lg text-red-500">{errorMessage}</p>
+
+          <button onClick={() => handleButtonClick()} type="submit" className="bg-red-700 p-4 my-2 w-full rounded-lg opacity-100 text-white">{isSignInForm ? "Sign In" : "Sign Up"}</button>
+         
           <p className="text-white my-4 p-4 cursor-pointer" onClick={() => toggleSignInForm()}>{isSignInForm ? "New to Netflix? Sign up now." : "Already a user? Sign In now."}</p>
         </form>
     </div>
